3ab878d2aaab9730bf4ed2969653d3a23b3ff19e
VelikovPetar <petarvelikov95@gmail.com>
2020-02-15 19:51:29 +0100

    Create load-test script.

--- load-test/src/main/kotlin/load_test.kt
+++ load-test/src/main/kotlin/load_test.kt
@@ -15,10 +15,10 @@ val OBJECT_MAPPER = ObjectMapper()
 val JSON = "application/json; charset=UTF-8".toMediaType()
 val MUTLI_PART = "multipart/form-data".toMediaType()
 
-const val REGISTER_URL = "http://192.168.39.130:30004/register"
-const val LOGIN_URL = "http://192.168.39.130:30004/login"
-const val EXECUTE_JAVA_URL = "http://192.168.39.115:31504/executeJava"
-const val EXECUTE_PYTHON_URL = "http://192.168.39.115:31504/executePython"
+const val REGISTER_URL = "http://192.168.39.172:32299/register"
+const val LOGIN_URL = "http://192.168.39.172:32299/login"
+const val EXECUTE_JAVA_URL = "http://192.168.39.172:30012/executeJava"
+const val EXECUTE_PYTHON_URL = "http://192.168.39.172:30012/executePython"
 
 const val PYTHON_SOLUTION_FILE_PATH = "/home/petarv/Documents/diplomska/load-test/src/main/kotlin/solution.py"
 const val JAVA_SOLUTION_FILE_PATH = "/home/petarv/Documents/diplomska/load-test/src/main/kotlin/Test.java"
@@ -94,7 +94,7 @@ fun execute(
 fun createRegistrations(numberOfRegistrations: Int): List<Register> {
   val registrations = mutableListOf<Register>()
   for (i in 1..numberOfRegistrations) {
-    registrations.add(Register("name$i", "surname$i", "user$i@mail.com", "password$i"))
+    registrations.add(Register("name$i-1", "surname$i-1", "user$i@mail.com-1", "password$i-1"))
   }
   return registrations
 }
@@ -103,68 +103,133 @@ fun createLogins(numberOfLogins: Int): List<Login> = createRegistrations(numberO
   Login(it.email, it.password)
 }
 
-
-//fun main(args: Array<String>) {
-//  println("Hello misiri")
-//  createRegistrations(2000).forEach {
-//    GlobalScope.launch {
-//      val response = register(it)
-//      println("Registration of user $it complete with result: ${response.responseCode}/${response.errorMessage}")
-//    }
-//  }
-//  createLogins(2000).forEach {
-//    GlobalScope.launch {
-//      val pair = loginAndExtractToken(it)
-//      if (pair.second == null) {
-//        println("Failed to login user $it with result: ${pair.first.responseCode}/${pair.first.errorMessage}")
-//      } else {
-//        println("Login success for user $it with token: ${pair.second}")
-//      }
-//    }
-//  }
-//  createLogins(200).forEach {
-//    GlobalScope.launch {
-//      val loginResponse = loginAndExtractToken(it)
-//      if (loginResponse.second != null) {
-//        val exResponse = execute(EXECUTE_JAVA_URL, 1118, loginResponse.second!!, "Test.java", File(JAVA_SOLUTION_FILE_PATH))
-////        val exResponse = execute(EXECUTE_PYTHON_URL, 1118, loginResponse.second!!, "solution.py", File(PYTHON_SOLUTION_FILE_PATH))
-//        println(exResponse)
-//      } else {
-//        println("Login failed for user $it with code ${loginResponse.first.responseCode} and message ${loginResponse.first.errorMessage}")
-//      }
-//    }
-//  }
-//  Thread.sleep(2 * 60 * 1_000)
-//}
-
 open class App {
-  companion object {
-    @JvmStatic
-    fun main(args: Array<String>) {
-//      createRegistrations(2000).forEach {
-//        GlobalScope.launch {
-//          val response = register(it)
-//          println("Registration of user $it complete with result: ${response.responseCode}/${response.errorMessage}")
-//        }
-//      }
 
-      val n = 60
-      for (i in 0..n) {
-        println("Executing batch $i/$n: Executing 10 000 login requests...")
-        createLogins(10000).forEach {
-          println(it)
+  companion object {
+
+    private enum class Action(val value: String) {
+      REGISTRATION("reg"),
+      AUTHENTICATION("auth"),
+      EVALUATION("eval")
+    }
+
+    private fun performRegistrations(n: Int) {
+      println("Performing $n registrations...")
+      createRegistrations(n).forEach { reg ->
+        GlobalScope.launch {
+          val response = register(reg)
+          println("Registration of user $reg complete with result: ${response.responseCode}/${response.errorMessage}")
+        }
+      }
+      Thread.sleep(2 * 60 * 1000)
+    }
+
+    private fun performLogins(n: Int, duration: Int) {
+      for (i in 0..duration) {
+        createLogins(n).forEach { login ->
           GlobalScope.launch {
-            val pair = loginAndExtractToken(it)
-//            if (pair.second == null) {
-//              println("Failed to login user $it with result: ${pair.first.responseCode}/${pair.first.errorMessage}")
-//            } else {
-//              println("Login success for user $it with token: ${pair.second}")
-//            }
+            val loginResult = loginAndExtractToken(login)
+            if (loginResult.second == null) {
+              println("Failed to login user $login with result: ${loginResult.first.responseCode}/${loginResult.first.errorMessage}")
+            } else {
+              println("Login success for user $login with token: ${loginResult.second}")
+            }
           }
         }
-        Thread.sleep(1000)
+        Thread.sleep(1_000)
       }
-      Thread.sleep(5_000)
+      Thread.sleep(2 * 60 * 1000)
+    }
+
+    private fun performEvaluations(n: Int, duration: Int) {
+      for (i in 0..duration) {
+        createLogins(n).forEach { login ->
+          GlobalScope.launch {
+            val loginResult = loginAndExtractToken(login)
+            if (loginResult.second == null) {
+              val exResponse = execute(EXECUTE_JAVA_URL, 1118, loginResult.second!!, "Test.java", File(JAVA_SOLUTION_FILE_PATH))
+              println(exResponse)
+            } else {
+              println("Login success for user $login with token: ${loginResult.second}")
+            }
+          }
+        }
+        Thread.sleep(1_000)
+      }
+      Thread.sleep(2 * 60 * 1000)
+    }
+
+    private fun handleRegistration(args: Array<String>) {
+      if (args.size < 2) {
+        println("No number of requested registrations entered.")
+        return
+      }
+      val n = args[1].toIntOrNull()
+      if (n == null) {
+        println("Invalid number of requested registrations entered.")
+        return
+      }
+      performRegistrations(n)
+    }
+
+    private fun handleAuthentication(args: Array<String>) {
+      if (args.size < 2) {
+        println("No number of requested logins/second entered.")
+        return
+      }
+      val n = args[1].toIntOrNull()
+      if (n == null) {
+        println("Invalid number of requested logins/second entered.")
+        return
+      }
+      if (args.size < 3) {
+        println("No duration entered (seconds) entered.")
+        return
+      }
+      val duration = args[2].toIntOrNull()
+      if (duration == null) {
+        println("No valid duration entered (seconds) entered.")
+        return
+      }
+      performLogins(n, duration)
+    }
+
+    private fun handleEvaluation(args: Array<String>) {
+      if (args.size < 2) {
+        println("No number of requested logins/second entered.")
+        return
+      }
+      val n = args[1].toIntOrNull()
+      if (n == null) {
+        println("Invalid number of requested logins/second entered.")
+        return
+      }
+      if (args.size < 3) {
+        println("No duration entered (seconds) entered.")
+        return
+      }
+      val duration = args[2].toIntOrNull()
+      if (duration == null) {
+        println("No valid duration entered (seconds) entered.")
+        return
+      }
+      performEvaluations(n, duration)
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+      if (args.isEmpty()) {
+        println("No action provided. Please provide one of the following: [${Action.values().joinToString(separator = ", ") { it.value }}]")
+        return
+      }
+      when (args[0]) {
+        Action.REGISTRATION.value -> handleRegistration(args)
+        Action.AUTHENTICATION.value -> handleAuthentication(args)
+        Action.EVALUATION.value -> println("Executing authentication+evaluation...")
+        else -> {
+          println("Invalid action provided. Please provide one of the following: [${Action.values().joinToString(separator = ", ") { it.value }}]")
+        }
+      }
     }
   }
 }
